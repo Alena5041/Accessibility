a714e31fe921a88792f63c54c3f11b88bcc021b7
Alena Skorynina <41771448+Alena5041@users.noreply.github.com>
2022-07-29 12:57:16 +0200

    add description

--- inaccessible-components/pages/page-modal-window.js
+++ inaccessible-components/pages/page-modal-window.js
@@ -26,54 +26,78 @@ export default function PageModal() {
           <summary>
             Non-usage <code>role="dialog"</code> for modal.
           </summary>
+          The dialog role is used to mark up an HTML based application dialog or
+          window that separates content or UI from the rest of the web
+          application or page.
         </details>
 
         <details>
           <summary>
             Non-usage <code>aria-labelledby</code> for modal.
           </summary>
+          If a dialog already has a visible title bar, the text inside that bar
+          can be used to label the dialog itself by using the aria-labelledby
+          attribute.
         </details>
 
         <details>
           <summary>
             Non-usage <code>aria-describedby</code> for modal.
           </summary>
+          if the dialog contains additional descriptive text besides the dialog
+          title, this text can be associated with the dialog using the
+          aria-describedby attribute.
         </details>
 
         <details>
           <summary>Small size of close button.</summary>
+          When people interact with small buttons, it's very likely they might
+          accidentally miss the button.
         </details>
 
         <details>
           <summary>Close button without padding.</summary>
+          Adding padding can help to do bigger zone for clicking.
         </details>
 
         <details>
           <summary>Close button don't have focus style.</summary>
+          The key purpose of focus is to give a user guidance.
         </details>
 
         <details>
           <summary>
             Non-usage <code>aria-label</code> for close button.
           </summary>
+          The aria-label should be used to provide a text alternative to an
+          element that has no visible text on the screen.
         </details>
 
         <details>
           <summary>
             Non-usage <code>aria-hidden</code> for close icon.
           </summary>
+          The hidden attribute tells browsers not to display the element. The
+          aria-hidden property tells screen-readers if they should ignore the
+          element.
         </details>
 
         <details>
-          <summary>Modal has fixed width in px</summary>
+          <summary>Modal has fixed width in px.</summary>
+          The most important reason for using responsive and unitless values in
+          CSS is for supporting our users that rely on zooming.
         </details>
 
         <details>
-          <summary>Focus don't work properly</summary>
+          <summary>Focus don't work properly.</summary>
+          To make the content easier to navigate when modal is open leave focus
+          only for elements inside.
         </details>
 
         <details>
-          <summary>Non-usage responsive design</summary>
+          <summary>Non-usage responsive design.</summary>
+          To make the content easier to read when displayed on small screens,
+          the dialog fills 100% of the screen.
         </details>
 
         <h2>Accessible version</h2>
